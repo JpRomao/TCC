fb74b92d297c63e54d9a5be177eaa3d0d945e526
João Pedro Romão <ojpzinho@gmail.com>
2020-12-18 13:19:56 -0300

    Update Application.php

--- Application.php
+++ Application.php
@@ -1,15 +1,15 @@
 <?php
-    define('INCLUDE_PATH_ROOT', 'http://localhost/TCC/');
-    define('INCLUDE_PATH_VIEWS', 'http://localhost/TCC/Views/');
     class Application{
         public function execute(){
+            echo 'Chega em Application<br>';
             $url = isset($_GET['url']) ? explode('/',$_GET['url'])[0] : 'Home';
-
+            
             $url = ucfirst($url);
             $url .= "Controller";
-
-            if(file_exists('./Controllers/'.$url.'.php')){
-                $className = 'Controllers\\'.$url;
+            
+            if(file_exists('Controllers/'.$url.'.php')){
+                $className = 'Controllers/'.$url;
+                require_once $className.'.php';
                 $controller = new $className;
                 $controller->execute();
             }
