34e6352e962dc9f4e05a18114a1b6c4a7b2e6cfd
João Pedro da Silva Romão <ojpzinho@gmail.com>
2020-12-17 23:40:46 -0300

    form-aluno melhorado

--- Views/Pages/FormAluno/formStudent.php
+++ Views/Pages/FormAluno/formStudent.php
@@ -2,34 +2,46 @@
     <div class="content">
         <div id="form">
             <div class="form-info">
-                <div class="labels">
-                    <label for="nome">Nome: </label>
-                    <label for="sobrenome">Sobrenome: </label>
-                    <label for="turma">Turma: </label>
-                    <label for="ano">Ano: </label>
-                    <label for="prontuario">Prontuário: </label>
-                </div>
                 <div class="campos">
-                    <input type="text" name="nome" id="nome" required="required"/>
-                    <input type="text" name="sobrenome" id="sobrenome" required="required"/>
-                    <select name="turma" id="turma" required="required">
-                        <option value="" selected="selected" hidden="hidden">
-                            &lt;Turmo do aluno&gt;
-                        </option>
-                        <option value="Informática">Informática</option>
-                        <option value="Mecânica">Mecânica</option>
-                    </select>
-                    <select name="ano" id="ano" required="required">
-                        <option value="" selected="selected" hidden="hidden">
-                            &lt;Ano que o aluno está cursando&gt;
-                        </option>
-                        <?php
-                            for($i=1;$i<=4;$i++){
-                                echo "<option value='$i'>".$i."° Ano</option>";
-                            }
-                        ?>
-                    </select>
-                    <input type="number" name="prontuario" id="prontuario" required="required"/>
+                    <div class="input-block">
+                        <label for="nome">
+                            Nome:
+                        </label>
+                            <input type="text" name="nome" id="nome" required="required"/>
+                    </div>
+                    <div class="input-block">
+                        <label for="sobrenome">
+                            Sobrenome:
+                        </label>
+                            <input type="text" name="sobrenome" id="sobrenome" required="required"/>
+                    </div>
+                    <div class="input-block">
+                        <label for="turma">Turma: </label>
+                            <select name="turma" id="turma" required="required">
+                                <option value="" selected="selected" hidden="hidden">
+                                    &lt;Turmo do aluno&gt;
+                                </option>
+                                <option value="Informática">Informática</option>
+                                <option value="Mecânica">Mecânica</option>
+                            </select>
+                    </div>
+                    <div class="input-block">
+                        <label for="ano">Ano: </label>
+                            <select name="ano" id="ano" required="required">
+                                <option value="" selected="selected" hidden="hidden">
+                                    &lt;Ano do aluno&gt;
+                                </option>
+                                <?php
+                                    for($i=1;$i<=4;$i++){
+                                        echo "<option value='$i'>".$i."° Ano</option>";
+                                    }
+                                ?>
+                            </select>
+                    </div>
+                    <div class="input-block">
+                        <label for="prontuario">Prontuário: </label>
+                            <input type="number" name="prontuario" id="prontuario" required="required"/>
+                    </div>
                 </div>
             </div>
             <button type="submit" id="action" class="btn">Registrar</button>
