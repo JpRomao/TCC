d55ce760008d370f0d6454e5e0021754fb23d462
João Pedro da Silva Romão <ojpzinho@gmail.com>
2020-12-17 23:01:12 -0300

    bar link bug fixed

--- connection.php
+++ connection.php
@@ -4,7 +4,7 @@
     try{
         $pdo = new PDO("mysql:host=".DATABASE['HOST'].";dbname=".DATABASE['DB'],DATABASE['USER'],DATABASE['PASSWORD'],
                 array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-        $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); //DEV MODE
+        //$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); //DEV MODE
     }catch(Exception $e){
         echo $e->getMessage();
     }
