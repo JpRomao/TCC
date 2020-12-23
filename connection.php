0d01675e1aeafc9df6376b157b1f65f71067d9fc
Joao Pedro Romao <ojpzinho@gmail.com>
2020-12-23 01:38:38 -0300

    database

--- connection.php
+++ connection.php
@@ -1,10 +1,9 @@
 <?php
-    define('DATABASE',array('HOST'=>'https://databases-auth.000webhost.com/','DB'=>'id15626848_ifbooks','USER'=>'id15626848_edujoanat21','PASSWORD'=>'gZuG9R)Yq^k7wB6$'));
+    define('DATABASE',array('HOST'=>'db4free.net','DB'=>'ifbooks','USER'=>'edujoanat21','PASSWORD'=>'gZuG9R)Yq^k7wB6$'));
 
     try{
-        $pdo = new PDO("mysql:host=".DATABASE['HOST'].";dbname=".DATABASE['DB'],DATABASE['USER'],DATABASE['PASSWORD'],
+        $pdo = new PDO("mysql:host=".DATABASE['HOST'].";port=3306;dbname=".DATABASE['DB'],DATABASE['USER'],DATABASE['PASSWORD'],
                 array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-        //$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); //DEV MODE
     }catch(Exception $e){
         echo $e->getMessage();
     }
