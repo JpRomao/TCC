3e25238b4cf36cb6c445307a2cc84897c44e0c8d
Joao Pedro Romao <ojpzinho@gmail.com>
2020-12-23 01:46:03 -0300

    database

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
     define('DATABASE',array('HOST'=>'85.10.205.173','DB'=>'ifbooks','USER'=>'edujoanat21','PASSWORD'=>'gZuG9R)Yq^k7wB6$'));
 
     try{
-        $pdo = new PDO("mysql:host=".DATABASE['HOST'].";port=3307;dbname=".DATABASE['DB'],DATABASE['USER'],DATABASE['PASSWORD'],
+        $pdo = new PDO("mysql:host=".DATABASE['HOST'].";port=3308;dbname=".DATABASE['DB'],DATABASE['USER'],DATABASE['PASSWORD'],
                 array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
     }catch(Exception $e){
         echo $e->getMessage();
