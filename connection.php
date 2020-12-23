c712206cc3903bb91ee160f46a2392902cf6994d
Joao Pedro Romao <ojpzinho@gmail.com>
2020-12-23 02:40:10 -0300

    database

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-    define('DATABASE',array('HOST'=>'databases.000webhost.com','DB'=>'id15626848_ifbooks','USER'=>'id15626848_edujoanat21','PASSWORD'=>'gZuG9R)Yq^k7wB6$'));
+    define('DATABASE',array('HOST'=>'85.10.205.173','DB'=>'ifbooks','USER'=>'edujoanat21','PASSWORD'=>'gZuG9R)Yq^k7wB6$'));
 
     try{
         $pdo = new PDO("mysql:host=".DATABASE['HOST'].";port=3306;dbname=".DATABASE['DB'],DATABASE['USER'],DATABASE['PASSWORD'],
