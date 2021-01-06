c218654177207ad05e41cd833475f89562b2b9c5
Joao Pedro Romao <ojpzinho@gmail.com>
2021-01-05 23:40:03 -0300

    ff

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-    define('DATABASE',array('HOST'=>'db4free.net', 'PORT'=>'3306', 'DB'=>'ifbooks', 'USER'=>'edujoanat', 'PASSWORD'=>'gZuG9R)Yq^k7wB6$'));
+    define('DATABASE',array('HOST'=>'localhost', 'PORT'=>'3306', 'DB'=>'ifbooks', 'USER'=>'root', 'PASSWORD'=>''));
 
     try{
         $pdo = new PDO("mysql:host=".DATABASE['HOST'].";port=".DATABASE['PORT'].";dbname=".DATABASE['DB'],DATABASE['USER'],DATABASE['PASSWORD']);
