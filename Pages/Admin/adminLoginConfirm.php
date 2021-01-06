89f66748f206f19c2150269b4ef52f844052b210
Joao Pedro Romao <ojpzinho@gmail.com>
2021-01-05 23:28:38 -0300

    login consertado

--- Pages/Admin/adminLoginConfirm.php
+++ Pages/Admin/adminLoginConfirm.php
@@ -14,20 +14,17 @@
 
     $sql = $pdo->prepare("SELECT * FROM administradores WHERE login = ?");
     $sql->execute($user);
-    echo "select";
 
     if($sql->rowCount() === 1){
-        echo "<br/>select bem sucedido";
         $info = $sql->fetch();
 
         if($password === $info['senha']){
-            echo "<br/>senha";
             $_SESSION['admin'] = true;
             $_SESSION['id'] = $info['id'];
             $_SESSION['user'] = $info['user'];
         }
     }
 
-    die();
     header("Location: https://ifbookst.herokuapp.com/");
+    die();
 ?>
